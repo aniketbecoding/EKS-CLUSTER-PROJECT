09f3151a4a31bbf578d3180be0b559c73251c0e5
aniketbecoding <83643436+aniketbecoding@users.noreply.github.com>
2024-11-17 20:35:08 +0530

    Add files via upload

---
+++ EKS_Cluster_Project/variables.tf
@@ -0,0 +1,6 @@
+variable "kubernetes_version" {
+  default = 1.27
+}
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
---
+++ EKS_Cluster_Project/vpc.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "aniket-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.15.0"
+
+  name                 = "aniket-eks-vpc"
+  cidr                 = var.vpc_cidr
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+
+
+}
